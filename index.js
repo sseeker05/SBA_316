3f965ac9230be565f94d47cfa0965e6c8702b660
sseeker05 <sseeker05@gmail.com>
2024-10-21 15:31:52 -0400

    dom

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const changeFont = document.getElementById("TXT");
 changeFont.style.fontSize = "30px"
 
@@ -9,15 +11,38 @@ document.body.appendChild(newItem)
 
 document.querySelector("h1").innerHTML="Connecting 60's Dating Site"
 
+const form = document.getElementById("signup-form")
+const username = form.elements["username"]
+const email = form.elements["email"]
+const password = form.elements["password"]
+const age = form.elements["age"]
 
 
+function validateForm(e) {
+e.preventDefault()
+  const usernameInput = username.value
+ const passwordInput = password.value
+ const emailInput = email.value
+ const ageInput = age.value
+  
+  if (usernameInput === "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (passwordInput === "") {
+    alert("Password must be filled out");
+    return false;
+  }
+  if (emailInput === "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  if (ageInput === "") {
+    alert("Age must be filled out");
+    return false;
+  }
+
+  
+}
 
 
-
-
-// const form = document.getElementById("signup-form")
-
-
-// form.addEventListener("summit", (e) => {
-//   e.preventDefault()
-// })
