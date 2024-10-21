935090d32098e65f91f7aac2f480b092fe3f375c
sseeker05 <sseeker05@gmail.com>
2024-10-20 22:20:44 -0400

    dom

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const form = document.getElementById("signup-form")
+const changeFont = document.getElementById("TXT");
+changeFont.style.fontSize = "30px"
 
-form.addEventListener("summit", (e) => {
-  e.preventDefault()
-})
+document.querySelector("p").style.fontSize = "20px";
+
+const newItem = document.createElement("h1")
+newItem.innerText = "DateMyAge"
+document.body.appendChild(newItem)
+
+
+
+
+
+
+
+
+
+// const form = document.getElementById("signup-form")
+
+
+// form.addEventListener("summit", (e) => {
+//   e.preventDefault()
+// })
